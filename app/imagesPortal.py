e25a51b97b0e49a3402c4385cf57cf0099d050cd
Carlos Claro em POW <carlosclaro79@gmail.com>
2019-06-28 14:12:39 -0300

    implementa portal

---
+++ app/imagesPortal.py
@@ -0,0 +1,100 @@
+import os
+from PIL import Image
+from resizeimage import resizeimage
+import requests
+import os.path
+import time
+
+# https://github.com/charlesthk/python-resize-image
+class imagesPortal(object):
+
+    def __init__(self):
+        self.pasta_cwd = '/var/www/html/images/portais/'
+
+    def executa(self,image,nome, caminho):
+        for tamanho in self.tamanhos():
+            self.geraImages(image,nome,tamanho, caminho)
+
+    def get_extensao_original(self, mime):
+        if 'jpeg' in mime:
+            return 'jpg'
+        elif 'png' in mime:
+            return 'png'
+    
+    def verificaPasta(self,id_empresa):
+        if os.path.isdir(self.pasta_cwd + id_empresa):
+            if os.path.isdir(self.pasta_cwd + id_empresa + '/originais'):
+                return True
+            else:
+                os.mkdir(self.pasta_cwd + id_empresa + '/originais')
+                return self.verificaPasta(id_empresa)
+        else:
+            os.mkdir(self.pasta_cwd + id_empresa)
+            return self.verificaPasta(id_empresa)
+    
+    def setArquivo(self,image):
+        if 'http' in image['arquivo'] :
+            return image['arquivo']
+        else:
+            return 'http://pow.com.br/powsites/' + str(image['id_empresa']) + '/imo/650F_' + image['arquivo']
+        
+    
+    def copyImage(self,image):
+        inicio = time.time()
+        a = self.setArquivo(image)
+        res = requests.get(a, stream=True)
+        content_type = res.headers['content-type']
+        if res.status_code == 200 and not 'html' in content_type:
+            id_empresa = str(image['id_empresa'])
+            self.verificaPasta(id_empresa)
+            extensao = self.get_extensao_original(content_type)
+            nome_arquivo = '{}_{}.{}'.format(image['id_imovel'],image['id'], extensao)
+            caminho = self.pasta_cwd + id_empresa + '/'
+            with open(caminho + 'originais/' + nome_arquivo, 'wb') as f:
+                f.write(res.content)
+            self.executa(caminho + 'originais/' + nome_arquivo, nome_arquivo, caminho)
+            arquivo_destaque = caminho + 'destaque_' + nome_arquivo
+            os.unlink(caminho + 'originais/' + nome_arquivo)
+            if os.path.exists(arquivo_destaque):
+                fim = time.time()
+                print(fim-inicio)
+                return extensao
+            else:
+                fim = time.time()
+                print(fim-inicio)
+                return False
+        else:
+            fim = time.time()
+            print(fim-inicio)
+            return False
+
+    def geraImages(self,image,nome,tamanho, caminho):
+        pa = caminho + tamanho['prefixo'] + nome
+        if os.path.exists(pa) == False:
+            with Image.open(image) as imagem:
+                print(tamanho['width'])
+                print(imagem.width)
+                if tamanho['width'] == 'auto' :
+                    cover = resizeimage.resize_width(imagem,imagem.size[0])
+                elif tamanho['width'] > imagem.width :
+                    cover = resizeimage.resize_width(imagem,imagem.size[0])
+                else:
+                    cover = resizeimage.resize_width(imagem,tamanho['width'])
+                cover.save(caminho + tamanho['prefixo'] + nome, 'jpeg')
+
+    def tamanhos(self):
+        tamanho = [
+                    {'width':300,       'height':'auto', 'prefixo':'destaque_'},
+                    {'width':650,       'height':'auto', 'prefixo':'vitrine_'},
+                    {'width':'auto',    'height':'auto', 'prefixo':'ampliado_'},
+                ]
+        return tamanho
+
+
+if __name__ == '__main__':
+    try:
+        imagesPortal()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Finaliza myPNG")
---
+++ app/portal.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import requests
+import shutil
+from imagesPortal import imagesPortal
+import datetime
+import time
+import os
+
+class Imoveis(object):
+    
+    def __init__(self):
+        self.inicio = time.time()
+        self.URI = 'http://localhost:5000/'
+        #self.URI = 'http://201.16.246.176:5000/'
+        self.URL_GET = self.URI + 'imoveis_images_gerar/100'
+        self.URL_PUT = self.URI + 'imovel_images/'
+        self.images = imagesPortal()
+
+    def main(self):
+        if os.path.exists('rodando.txt') :
+            print('rodando')
+        else:
+            with open('rodando.txt', 'w') as f:
+                f.write('rodando')
+            images = requests.get(self.URL_GET)
+            i = images.json()
+            for v in i:
+                print('id - ' + str(v['id']) + ' - ' + v['arquivo'])
+                res = self.images.copyImage(v)
+                if res:
+                    data = {'extensao': str(res), 'data':datetime.datetime.now().strftime('%Y-%m-%d %H:%M'), 'gerado_image' : 1}
+                    update = requests.put(self.URL_PUT + str(v['id']),params=data)
+                    print('res sucesso')
+                    print(data)
+                    print(self.URL_PUT + str(v['id']))
+                    print(update.status_code)
+                    print(update.content)
+                else:
+                    print('res fail')
+                    print(v)
+                    data = {'data':datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),'gerado_image':2}
+                    print(data)
+                    update = requests.put(self.URL_PUT + str(v['id']),params=data)
+            self.fim = time.time()
+            print(self.fim-self.inicio)
+            os.unlink('rodando.txt')
+            
+        
+    
+if __name__ == '__main__':
+    Imoveis().main()
