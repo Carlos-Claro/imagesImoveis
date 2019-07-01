6649b8af8bab6f588329cced83db0629b1017cb1
Carlos Claro em POW <carlosclaro79@gmail.com>
2019-07-01 14:24:48 -0300

    update imovel quando imagem gerada

--- app/portal.py
+++ app/portal.py
@@ -10,10 +10,11 @@ class Imoveis(object):
     
     def __init__(self):
         self.inicio = time.time()
-        #self.URI = 'http://localhost:5000/'
-        self.URI = 'http://201.16.246.176:5000/'
+        self.URI = 'http://localhost:5000/'
+        #self.URI = 'http://201.16.246.176:5000/'
         self.URL_GET = self.URI + 'imoveis_images_gerar/100'
         self.URL_PUT = self.URI + 'imovel_images/'
+        self.URL_PUT_IMOVEL = self.URI + 'imovel/'
         self.URL_rodando = '/var/www/html/images/'
         self.images = imagesPortal()
 
@@ -31,11 +32,16 @@ class Imoveis(object):
                 if res:
                     data = {'extensao': str(res), 'data':datetime.datetime.now().strftime('%Y-%m-%d %H:%M'), 'gerado_image' : 1}
                     update = requests.put(self.URL_PUT + str(v['id']),params=data)
+                    data_imovel = {'integra_mongo_db':"0000-00-00"}
+                    update_imovel = requests.put(self.URL_PUT_IMOVEL + str(v['id_imovel']),params=data_imovel)
                     print('res sucesso')
                     print(data)
                     print(self.URL_PUT + str(v['id']))
+                    print(self.URL_PUT_IMOVEL + str(v['id_imovel']))
                     print(update.status_code)
                     print(update.content)
+                    print(update_imovel.status_code)
+                    print(update_imovel.content)
                 else:
                     print('res fail')
                     print(v)
